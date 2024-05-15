5e53f2ed74552be83d3bb919d8fba2debc695307
Oleg <logardyt@gmail.com>
2024-05-15 10:24:34 +0700

    init

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.widget import get_new_data, mask_account_card
+
+print(mask_account_card("Visa Platinum 7000792289606361"))
+
+print(mask_account_card("Счет 73654108430135874305"))
+
+print(get_new_data("2018-07-11T02:26:18.671407"))
---
+++ src/masks.py
@@ -0,0 +1,11 @@
+def mask_card(number: str) -> str:
+    """Функция принимает строку и возвращаем маскировку карты"""
+
+    new_string = f"{number[0:4]} {number[4:6]}** **** {number[12:]}"
+    return new_string
+
+
+def mask_account(acc_number: str) -> str:
+    """Функция принимает строку и возвращаем маскировку счета"""
+    new_string = f"** {acc_number[-4:]}"
+    return new_string
